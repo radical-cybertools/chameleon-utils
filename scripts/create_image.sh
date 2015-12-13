b427cf03b293747cbafa0b635e704fd906b81d40
Andre Luckow <andre.luckow@gmail.com>
2015-12-13 15:40:03 +0000

    update create image script

--- scripts/create_image.sh
+++ scripts/create_image.sh
@@ -10,4 +10,4 @@ sudo virt-make-fs --format=qcow2 --type=ext4 --label=img-rootfs /tmp/snapshot.ta
 sudo virt-sysprep -a /tmp/snapshot.qcow2
 sudo qemu-img convert /tmp/snapshot.qcow2 -O qcow2 /tmp/snapshot_compressed.qcow2 -c
 
-glance image-create --name CentOS7-HDP2.3-${now}-snapshot --disk-format qcow2 --container-format bare --property kernel_id=a0aa7bde-c5a3-4180-b64d-0032e2da168e --property ramdisk_id=b39feeff-63f5-44ad-b1ff-7cf70d1a4309 < /tmp/snapshot_compressed.qcow2
+#glance image-create --name CentOS7-HDP2.3-${now}-snapshot --disk-format qcow2 --container-format bare --property kernel_id=a0aa7bde-c5a3-4180-b64d-0032e2da168e --property ramdisk_id=b39feeff-63f5-44ad-b1ff-7cf70d1a4309 < /tmp/snapshot_compressed.qcow2
